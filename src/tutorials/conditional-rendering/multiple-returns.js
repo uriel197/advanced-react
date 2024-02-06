b9ab5265f6756bfd48453a1bc82661056f6f914e
uriel197 <60386860+uriel197@users.noreply.github.com>
2024-02-06 10:57:23 +0200

    fetching data with multiple returns modified

--- src/tutorials/conditional-rendering/multiple-returns.js
+++ src/tutorials/conditional-rendering/multiple-returns.js
@@ -4,15 +4,15 @@ const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState("Default User");
+  const [user, setUser] = useState({}); /* 1 */
 
   const getUser = async () => {
     try {
       const response = await fetch(url);
       if (response.status >= 200 && response.status <= 299) {
         const user = await response.json();
-        const { login } = user;
-        setUser(login);
+        console.log(user);
+        setUser(user);
         setIsLoading(false);
       } else {
         setIsLoading(false);
@@ -42,10 +42,16 @@ const MultipleReturns = () => {
       </div>
     );
   }
+  const { login, avatar_url } = user; /* 2 */
   return (
-    <>
-      <h1>{user}</h1> {/* 1 */}
-    </>
+    <ul className="users">
+      <li>
+        <img src={avatar_url} alt={login} />
+        <div>
+          <h3>{login}</h3>
+        </div>
+      </li>
+    </ul>
   );
 };
 
@@ -53,6 +59,10 @@ export default MultipleReturns;
 
 /************* COMMENTS **************
 
-***1:  our return statement, {user} is accessing the value stored in the user state variable declared using useState().
+***1:  In our code, user is an object returned from the API, not an array. Therefore, in order to access its properties we must initialize our state to be an object:
+const [user, setUser] = useState({});
+Then, in your return statement, you can directly access the properties of the user object
+
+***2: in order to access these variables in our return statement we must define them outside the getUser(), otherwise we wont be able to access them.
 
 */
