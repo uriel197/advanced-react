41a2e006c997c6ec8a9d1437552ae574b91d2e20
uriel197 <60386860+uriel197@users.noreply.github.com>
2024-02-06 13:14:22 +0200

    toggle buttons activating or deactivating a component/ show-hide.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Start from "./tutorials/conditional-rendering/short-circuit";
+import Start from "./tutorials/conditional-rendering/show-hide";
 
 function App() {
   return (
--- src/tutorials/conditional-rendering/show-hide.js
+++ src/tutorials/conditional-rendering/show-hide.js
@@ -1,7 +1,37 @@
 import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide window size
+      </button>
+      {show || <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const changeSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", changeSize);
+    return () => {
+      window.removeEventListener("resize", changeSize);
+    };
+  });
+  return (
+    <>
+      <h3>Window</h3>
+      <p>{size}px</p>
+    </>
+  );
 };
 
 export default ShowHide;
